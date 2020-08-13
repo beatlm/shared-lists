9f6c37728609bd77eb6fdee8d7a0f8f5347b13c9
Beatriz Lopez <beatlm@gmail.com>
2020-08-13 11:44:53 +0200

    firebase hosting

--- src/app/application/auth/auth.service.ts
+++ src/app/application/auth/auth.service.ts
@@ -1,22 +1,29 @@
+import { AuthState } from './auth.state';
 import { AuthDataService } from './auth-data.service';
 import { Injectable } from '@angular/core';
-
+import { AuthStore } from './auth.store';
 
 @Injectable({ providedIn: 'root' })
-
 export class AuthService {
-  constructor(private authDataService:AuthDataService) {}
+  constructor(
+    private authDataService: AuthDataService,
+    private store: AuthStore
+  ) {}
 
   logout(): Promise<void> {
     return this.authDataService.logout();
   }
 
-  login(email: string, password: string): Promise<firebase.auth.UserCredential> {
+  login(
+    email: string,
+    password: string
+  ): Promise<firebase.auth.UserCredential> {
+    this.store.update({ uid: email });
+    this.store.update({email:email});
     return this.authDataService.login(email, password);
   }
 
   register(email: string, password: string) {
     return this.authDataService.register(email, password);
   }
-
 }
